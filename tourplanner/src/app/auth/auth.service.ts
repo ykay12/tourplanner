88ab928bb487cc5bd553b595088310b13573c3be
Jasmina Mojdehkar <jasminamojdehkar@yahoo.de>
2026-03-06 12:53:17 +0100

    added login and auth mocking

--- tourplanner/src/app/auth/auth.service.ts
+++ tourplanner/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+  isLoggedIn = signal(localStorage.getItem('loggedIn') === 'true')
+
+
+  login(username: String, password: String): Boolean {
+    const success = username === "test" && password === "123"
+
+    if (success) {
+      this.isLoggedIn.set(true)
+      localStorage.setItem('loggedIn', 'true')
+      return true
+    }
+    return false
+  }
+
+
+  logout(){
+    this.isLoggedIn.set(false)
+    localStorage.removeItem('loggedIn')
+  }
 }
--- tourplanner/src/app/login/login.component.ts
+++ tourplanner/src/app/login/login.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
+   username = signal('')
+   password = signal('')
+   errorMsg = signal('')
+
+   constructor(
+    private authService: AuthService,
+    private router: Router
+   ){}
+
+  onUsernameInput(event: Event): void{
+      const value = (event.target as HTMLInputElement).value
+      this.username.set(value)
+  }
+
+  onPasswordInput(event: Event): void{
+    const value = (event.target as HTMLInputElement).value
+    this.password.set(value)
+  }
+
+
+  onLogin() : void{
+    const succes: Boolean = this.authService.login(this.username(), this.password())
+
+    if(succes){
+      this.errorMsg.set('')
+      this.router.navigate(['/'])
+    } else{
+      this.errorMsg.set("Whoops, something went wrong. Please try again.")
+    }
+  }
 }
