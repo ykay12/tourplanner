1e9131a50f7ee12c01cdd898768a97c3a688b04a
Lena Gampenrieder <if24b255@technikum-wien.at>
2026-03-24 11:29:24 +0100

    naviagtion über sidebar

--- tourplanner/src/app/ui/loggedIn_shell/dashboard-home/dashboard-home.component.ts
+++ tourplanner/src/app/ui/loggedIn_shell/dashboard-home/dashboard-home.component.ts
@@ -1,9 +1,11 @@
 //dashboard-home.component.ts
 import { Component } from '@angular/core';
 
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard-home',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.scss'
 })
