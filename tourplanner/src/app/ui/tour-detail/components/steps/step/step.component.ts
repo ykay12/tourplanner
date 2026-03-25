ff4582cf5acd359af504aa104460ec522df40e04
Jasmina Mojdehkar <jasminamojdehkar@yahoo.de>
2026-03-25 14:10:07 +0100

    adapted styling for tour detail

--- tourplanner/src/app/ui/tour-detail/components/steps/step/step.component.ts
+++ tourplanner/src/app/ui/tour-detail/components/steps/step/step.component.ts
@@ -13,14 +13,15 @@ export class StepComponent {
 
   //nur eine gemockte Route damit ich beim designen sehen kann, wie ein Step aussieht
   @Input() step!: Route; //Aber welche Werte hat der jetzt?
-
+  @Input() stepNumber!: number;
+  
   getTransportIcon(): string {
-    switch(this.step.transportMode.toLowerCase()) {
+    switch (this.step.transportMode.toLowerCase()) {
       case 'bike': return 'bike.png';
       case 'walk': return 'walk.png';
       case 'run': return 'run.png';
       case 'hike': return 'hike.png';
-    default: return 'default.png';
+      default: return 'default.png';
     }
   }
 }
