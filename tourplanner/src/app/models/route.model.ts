9c4bdbbc4d94ed7ffbc48487d7f6f96792dfa4f0
Lena Gampenrieder <if24b255@technikum-wien.at>
2026-03-26 17:23:13 +0100

    export

--- tourplanner/src/app/models/route.model.ts
+++ tourplanner/src/app/models/route.model.ts
@@ -1,4 +1,4 @@
-type TransportMode = "Bike" | "Walk" | "Run"
+export type TransportMode = "Bike" | "Walk" | "Run"
 
 export interface Route{
     id: number, //Wir haben uns noch nicht geeinigt wie wir die ID anlegen wollen number/string/UUID?
