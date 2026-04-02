a4d4d5677807f1fcf2bc0c0ec7b3be8cda59ae28
Lena Gampenrieder <if24b255@technikum-wien.at>
2026-04-02 13:25:36 +0200

    Steps scrollable on Desktop and collapseable on mobile

--- tourplanner/src/app/ui/tour-detail/components/steps/steps.component.ts
+++ tourplanner/src/app/ui/tour-detail/components/steps/steps.component.ts
@@ -1,4 +1,3 @@
-
 //steps.component.ts
 
 /*
@@ -10,12 +9,14 @@ import { StepComponent } from './step/step.component';
 import { CommonModule } from '@angular/common';
 import { AppStateService } from '../../../../states/app-state.service';
 
+import { signal } from '@angular/core';
+
 @Component({
   selector: 'app-steps',
   standalone: true,
   imports: [CommonModule, StepComponent],
   templateUrl: './steps.component.html',
-  styleUrl: './steps.component.scss'
+  styleUrl: './steps.component.scss',
 })
 export class StepsComponent {
   constructor(@Inject(AppStateService) public state: AppStateService) {}
@@ -23,5 +24,12 @@ export class StepsComponent {
   // computed getter für selectedTour
   get tour() {
     return this.state.selectedTour();
+  }
+
+  //trying stuff for collapsing on mobile:
+  collapsed = signal(window.innerWidth < 768);
+
+  toggle() {
+    this.collapsed.update((v) => !v);
   }
 }
