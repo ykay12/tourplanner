21d8eb5d1ef2085d2fc27e48080ec54e1d07c164
Jasmina Mojdehkar <jasminamojdehkar@yahoo.de>
2026-04-06 12:19:10 +0200

    updated design

--- tourplanner/src/app/ui/loggedIn_shell/tours-overview-screen/tours-overview.component.ts
+++ tourplanner/src/app/ui/loggedIn_shell/tours-overview-screen/tours-overview.component.ts
@@ -29,7 +29,7 @@ export class ToursOverviewComponent {
   constructor(
     public state: AppStateService,
     private router: Router,
-  ) {}
+  ) { }
 
   ////////////////
   // FILTER-LOGIK
@@ -78,7 +78,7 @@ export class ToursOverviewComponent {
   /////////////////
   //ROUTING-LOGIK
   /////////////////
-  
+
   //Funktion damit beim klicken auf eine Tour im Overview die selectedTour im State verändert wird und wir zur TourDetail Route navigieren
   goToTour(tourId: number) {
     // 1. State setzen
